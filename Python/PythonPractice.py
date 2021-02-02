b0ffbba0109f95e9bf0308f9654c73352e432349
cerosilva <s.cicero@gmail.com>
2021-02-02 17:33:47 +0530

    a

--- Python/PythonPractice.py
+++ Python/PythonPractice.py
@@ -40,3 +40,5 @@ print('Have a nice day!')
 # If the number is a multiple of 4, print out a different message.
 # Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). 
 # If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+#Comment to test new branch
